c7f91e2ee65378e7651e450296a1fa01981d293e
jeonmingyun <jun.min.gyun@gmail.com>
2026-01-05 19:20:25 +0900

    dependencies 추가, 기본 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -33,10 +37,25 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
 
+    // Lifecycle
+//    implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4"
+//    implementation "androidx.lifecycle:lifecycle-runtime-ktx:2.8.4"
+
+    // Coroutines
+//    implementation "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1"
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // Default
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
