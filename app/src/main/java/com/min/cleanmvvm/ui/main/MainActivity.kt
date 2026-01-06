ca54d7317aadc5f66a9b7a424ac1e2ace4fbcb7f
jeonmingyun <jun.min.gyun@gmail.com>
2026-01-06 19:12:43 +0900

    error update : binding 선언 위치 수정

--- app/src/main/java/com/min/cleanmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/min/cleanmvvm/ui/main/MainActivity.kt
@@ -25,16 +25,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_main
-        )
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
